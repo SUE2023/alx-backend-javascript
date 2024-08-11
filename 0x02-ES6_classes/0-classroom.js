3ee0a319957e5718d507db4e747a6d83c048e177
Susan <susansewe@gmail.com>
2024-08-11 13:43:50 +0000

    Task 0

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,4 +1,9 @@
+// Represent a classroom.
 export default class ClassRoom {
+ /**
+  * It creates a new class linked to Classroom
+  * @parameter{Number} maxStudentsSize being maxim number in class
+  */
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
